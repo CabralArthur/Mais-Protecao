91aaff252c12575ad48be4e269970ef83cfd0258
Arthur Cabral <63428806+CabralArthur@users.noreply.github.com>
2021-09-03 23:38:28 -0300

    Removing Card

--- src/components/Sections/CardsContainer/index.jsx
+++ src/components/Sections/CardsContainer/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import SectionCard from '../../Elements/SectionCard'
 import { FaTaxi } from 'react-icons/fa'
-import { FaUber } from 'react-icons/fa'
 import { MdPhoneAndroid } from 'react-icons/md'
 import { Container, MainContainer, MainTitle } from './styles'
 
@@ -20,12 +19,6 @@ const CardsContainer = () => {
                 src="https://www.apvs.org.br/assets/images/xprotecao-moto.jpg.pagespeed.ic._3tyd9cUxl.webp"
                 component={ <MdPhoneAndroid/> }
                 title="Proteção Para Motoristas de App's"
-                text="Lorem ipsum dolor sit amet."/>
-                
-                <SectionCard
-                src="https://www.maxpixel.net/static/photo/1x/Transportation-Driver-Car-Taxi-Uber-Driving-Urban-4885228.jpg"
-                component={<FaUber/>}
-                title="Proteção Uber"
                 text="Lorem ipsum dolor sit amet."/>
             </Container>
         </MainContainer>
